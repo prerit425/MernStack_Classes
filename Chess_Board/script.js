a91d91df6d38637535fcf85600b5d6435b742fe9
prerit425 <159279873+prerit425@users.noreply.github.com>
2025-09-03 10:27:05 +0530

    Adding chess board

---
+++ Chess_Board/script.js
@@ -0,0 +1,31 @@
+// const board=document.getElementById("chessboard")
+
+// function createBoard(){
+//     for(let r=0;r<8;r++){
+//         for(let c=0;c<8;c++){
+//             const sq=document.createElement("div");
+//             sq.classList.add("sq");
+//             if((r+c)%2==0)
+//             {
+//                 sq.classList.add("white");
+//             }
+//             else{
+//                 sq.classList.add("black");
+//             }
+//             board.appendChild(sq);
+//         }
+//     }
+// }
+// createBoard(); 
+
+const board=document.getElementById("chessboard");
+function create(){
+    for(let r=0;r<8;r++)
+    {
+        for(let c=0;c<8;c++)
+        {
+            const sq=document.createElement("div");
+            s
+        }
+    }
+}
